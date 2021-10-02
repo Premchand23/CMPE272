fd1fbf75f49ddd924e35290cb1da3e6982e1a0ec
Premchand Purushothaman Jayachandran <premchandpurushothaman.jayachandran@sjsu.edu>
2021-10-01 20:41:45 -0700

    Files upload

---
+++ index.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>PremchandJ</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <!--<a class="navbar-brand" href="#page-top">Start Bootstrap</a> -->
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="#home">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#services">Servcies</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#news">News</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+                <div class="d-flex justify-content-center">
+                    <div class="text-center">
+                        <section class="home-section text-center" id="home"> 
+                            <h1 class="mx-auto my-0 text-uppercase">PremchandJ Auto</h1>
+                            <h2 class="text-white-50 mx-auto mt-2 mb-5">One stop solution for all your auto care.</h2>
+                            <a class="btn btn-primary" href="#about">Get Started</a>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- About-->
+        <section class="about-section text-center" id="about">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2 class="text-white mb-4">Your auto care specialist!</h2>
+                        <p class="text-white-50">
+                            We are your local auto care specialist and provide the best care your car deserves.
+                            <!--<a href="https://startbootstrap.com/theme/grayscale/">the preview page.</a>-->
+                            Trust us with you autocare and live without worries. 
+                        </p>
+                    </div>
+                </div>
+                <img class="img-fluid" src="assets/img/car1.jpg" alt="..." />
+            </div>
+        </section>
+        <!-- Projects-->
+        <section class="projects-section bg-light" id="services">
+            <div class="container px-4 px-lg-5">
+                <!-- Featured Project Row-->
+                <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="assets/img/car2.jpg" alt="..." /></div>
+                    <div class="col-xl-4 col-lg-5">
+                        <div class="featured-text text-center text-lg-left">
+                            <h4>General Car Care</h4>
+                            <p class="text-black-50 mb-0">Our services include full-service preventative maintenance and auto repair, providing comprehensive car care services for your domestic or import vehicle to keep it running at full power. We offer all of the standard services you would expect to protect your safety and the lifespan of your car.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Project One Row-->
+                <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/car3.jpg" alt="..." /></div>
+                    <div class="col-lg-6">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-left">
+                                    <h4 class="text-white">Expert Repair Servcies</h4>
+                                    <p class="mb-0 text-white-50">General Repair(Domesti and Import), Off-Road Vehicle Repair, Undercar Repair, Colling System services, A/C service, Manufacturer's recommended Maintanence, Transmission Servcies, Towing, and more...</p>
+                                    <hr class="d-none d-lg-block mb-0 ms-0" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Project Two Row-->
+                <!-- <div class="row gx-0 justify-content-center">
+                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-02.jpg" alt="..." /></div>
+                    <div class="col-lg-6 order-lg-first">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-right">
+                                    <h4 class="text-white">Mountains</h4>
+                                    <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
+                                    <hr class="d-none d-lg-block mb-0 me-0" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> -->
+        </section>
+        <section class="news-section text-center" id="news">
+            <div class="container px-4 px-lg-5">
+                   <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="assets/img/car4.jpg" alt="..." /></div>
+                    <div class="col-xl-4 col-lg-5">
+                        <div class="featured-text text-center text-lg-left">
+                            <h4>Car Care Checklist</h4>
+                            <p class="text-black-50 mb-0">Start the journey of being a car owner with this car maintenance checklist! Our general service schedule is easy to follow based on month intervals and focuses on what items need to be checked and when. Not only does routine auto care and preventative maintenance help keep your car safe and dependable, but it helps save money and aggravation. This infographic helps you understand what items should be inspected, either by you or your mechanic, to determine if there is a maintenance need before it becomes a bigger problem.</p>
+                        </div>
+                    </div>
+                </div>     
+            </div>
+            <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/car5.jpg" alt="..." /></div>
+                    <div class="col-lg-6">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-left">
+                                    <h4 class="text-white">Winter Maintanence Checklist</h4>
+                                    <p class="mb-0 text-white-50">Heavy snow can be fun for some, but many drivers dread treacherous winter driving conditions.
+
+                                    <ul class="mb-0 text-white-50">
+                                        <li>Battery – Cold weather is hard on batteries, so it’s wise to check the battery and charging system for optimum performance.</li>
+
+                                        <li>Antifreeze – Antifreeze (coolant) should be flushed and refilled at least every two years in most vehicles.
+                                        </li>
+
+                                        <li>Brakes – Have the brake system checked.</li>
+
+                                        <li>Tires – Check the tire tread depth and tire pressure, including the spare.</li>
+
+                                        <li>Oil – Be diligent about changing the oil at recommended intervals and check the fuel, air and transmission filters at the same time.</li>
+
+                                        <li>Lights & Wipers – Make sure all exterior and interior lights are working so you can see and be seen.</li>
+                                        </ul></p>
+                                    <hr class="d-none d-lg-block mb-0 ms-0" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </section>
+        <!-- Signup-->
+        <!--<section class="signup-section" id="signup">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-md-10 col-lg-8 mx-auto text-center">
+                        <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
+                        <h2 class="text-white mb-5">Subscribe to receive updates!</h2>
+                        * * * * * * * * * * * * * * *
+                         * * SB Forms Contact Form * *
+                         * * * * * * * * * * * * * * *
+                         This form is pre-integrated with SB Forms.
+                         To make this form functional, sign up at
+                         https://startbootstrap.com/solution/contact-forms
+                         to get an API token!
+                        <form class="form-signup" id="contactForm" data-sb-form-api-token="API_TOKEN">
+                             Email address input
+                            <div class="row input-group-newsletter">
+                                <div class="col"><input class="form-control" id="emailAddress" type="email" placeholder="Enter email address..." aria-label="Enter email address..." data-sb-validations="required,email" /></div>
+                                <div class="col-auto"><button class="btn btn-primary disabled" id="submitButton" type="submit">Notify Me!</button></div>
+                            </div>
+                            <div class="invalid-feedback mt-2" data-sb-feedback="emailAddress:required">An email is required.</div>
+                            <div class="invalid-feedback mt-2" data-sb-feedback="emailAddress:email">Email is not valid.</div>
+                            Submit success message
+                            
+                            This is what your users will see when the form
+                            has successfully submitted
+                            <div class="d-none" id="submitSuccessMessage">
+                                <div class="text-center mb-3 mt-2 text-white">
+                                    <div class="fw-bolder">Form submission successful!</div>
+                                    To activate this form, sign up at
+                                    <br />
+                                    <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                                </div>
+                            </div>
+                            Submit error message
+                            
+                            This is what your users will see when there is
+                             an error submitting the form
+                            <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3 mt-2">Error sending message!</div></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section> -->
+        <!-- Contact-->
+        <?php
+            $string = file_get_contents("data/contacts.txt");
+            $contact_arr = preg_split ("/\n/", $string);
+                    
+        ?>
+        <section class="contact-section bg-black" id="contact">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5">
+                    <?php foreach ($contact_arr as $contact) : ?>
+                    <?php
+                        
+                        $string_arr= preg_split ("/\;/", $contact);
+                        $name = $string_arr[0];
+                        $post = $string_arr[1];
+                        $phone = $string_arr[2];
+                        $email = $string_arr[3];
+                    
+                    ?>
+                    <div class="col-md-4 mb-3 mb-md-1">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-envelope text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Name</h4>
+                                <div class="small text-black-50"><a href="#!"><?= $name ?></a></div>
+                                <hr class="my-4 mx-auto" />
+                                <h4 class="text-uppercase m-0">Designation</h4>
+                                <div class="small text-black-50"><a href="#!"><?= $post ?></a></div>
+                                <hr class="my-4 mx-auto" />
+                                <h4 class="text-uppercase m-0">Phone Number</h4>
+                                <div class="small text-black-50"><a href="#!"><?= $phone ?></a></div>
+                                <hr class="my-4 mx-auto" />
+                                <h4 class="text-uppercase m-0">Email</h4>
+                                <div class="small text-black-50"><a href="#!"><?= $email ?></a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <?php endforeach ?>
+                    <!--
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-envelope text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Email</h4>
+                                <hr class="my-4 mx-auto" />
+                                <div class="small text-black-50"><a href="#!">hello@yourdomain.com</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-mobile-alt text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Phone</h4>
+                                <hr class="my-4 mx-auto" />
+                                <div class="small text-black-50">+1 (123) 456-7890</div>
+                            </div>
+                        </div>
+                    </div>
+                -->
+                </div>
+                <!--<div class="social d-flex justify-content-center">
+                    <a class="mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+                    <a class="mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+                    <a class="mx-2" href="#!"><i class="fab fa-github"></i></a>
+                </div>-->
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; PremchandJ 2021</div></footer\>
+        <a href="https://www.freepik.com/photos/car">Car photo created by azerbaijan_stockers - www.freepik.com</a>
+        <a href="https://www.freepik.com/photos/business">Business photo created by senivpetro - www.freepik.com</a>
+        <a href="https://www.freepik.com/photos/technology">Technology photo created by standret - www.freepik.com</a>
+        <a href="https://www.freepik.com/photos/business">Business photo created by jcomp - www.freepik.com</a>
+        <a href="https://www.freepik.com/photos/car">Car photo created by wirestock - www.freepik.com</a>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
