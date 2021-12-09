6b19af8c826fd9fa5a25eebc2182758f245835ab
Premchand Purushothaman Jayachandran <you@example.comclear>
2021-12-08 17:10:32 -0800

    Rating handler

---
+++ handlerating.php
@@ -0,0 +1,19 @@
+<?php
+
+  
+  session_start();
+  $connd = mysqli_connect("localhost","root","Leclerc@01","rating");
+if (mysqli_connect_errno()){
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+die();
+}
+  $product = $_POST['products'];
+  $service_rating = $_POST["rating"];
+  $service_review = $_POST["review"];
+  $sql="INSERT INTO rateservices (product,rating,review) VALUES ('".$product."','".$service_rating."','".$service_review."')";
+  
+  if($connd->query($sql) === TRUE){
+    header("Location: index.php");
+  } else {
+    header("Location: rating.php");
+  }
